f0174d8bf79d57c07b21ec560c6b67ae48bb9249
baiest <ballesteros.juan@correounivalle.edu.co>
2022-01-21 18:23:35 -0500

    Add punteros

---
+++ punteros/src/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type pc struct {
+	ram   int
+	disk  int
+	brand string
+}
+
+func (myPc pc) ping() {
+	fmt.Println(myPc.brand, "Pong")
+}
+
+func (myPc *pc) duplicateRam() {
+	myPc.ram *= 2
+}
+
+func (myPc pc) String() string {
+	return fmt.Sprintf("Tengo %d GB RAM, %d GB Disco y es una %s", myPc.ram, myPc.disk, myPc.brand)
+}
+
+func main() {
+	a := 50
+	b := &a
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(*b)
+
+	*b = 100
+	fmt.Println(*b)
+	fmt.Println(a)
+
+	myPc := pc{ram: 16, disk: 250, brand: "hp"}
+	myPc.ping()
+	myPc.duplicateRam()
+	fmt.Println(myPc)
+
+}
